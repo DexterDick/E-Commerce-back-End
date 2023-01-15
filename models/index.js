f98c291c538fda93721491c045cd2bf07f5102d9
Dexter Dick <@dexterdick@hotmail.com>
2023-01-15 16:35:19 -0500

    connect to a database using Sequelize and environment variable file

--- models/index.js
+++ models/index.js
@@ -1,20 +1,36 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+const { belongsToMany } = require("./Product");
 
 // Products belongsTo Category
-
-// Categories have many Products
-
+Product.belongsTo(Category, {
+    foreignKey: "category_id",
+    onDelete: "CASCADE",
+}),
+    // Categories have many Products
+    Category.hasMany(Product, {
+        foreignKey: "category_id",
+        onDelete: "CASCADE",
+    });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+    through: "productTag",
+    foreignKey: "product_id",
+    onDelete: "CASCADE",
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+    through: "productTag",
+    foreignKey: "tag_id",
+    onDelete: "CASCADE",
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
